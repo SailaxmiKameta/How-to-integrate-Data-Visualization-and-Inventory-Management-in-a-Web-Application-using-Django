d372b9d173096f263910090e4098908b4b103b87
SailaxmiKameta <Sailaxmi.Kameta@unh.edu>
2025-04-24 16:58:08 -0400

    Update Home page URL

--- InventoryViz/InventoryViz/urls.py
+++ InventoryViz/InventoryViz/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from inventory_dashboard.views import home  # Import the home view
 
 urlpatterns = [
-     
+    path('', home, name='home'), 
     path('admin/', admin.site.urls),
     path("auth/", include("inventory_dashboard.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
